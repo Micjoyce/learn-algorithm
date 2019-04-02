da11d27aaec741e07d9d51ff2c87c03a4d988187
micjoyce <micjoyce90@gmail.com>
2019-04-02 22:39:00 +0800

    tail factorial

--- factorial.js
+++ factorial.js
@@ -1,4 +1,5 @@
 // 递归计算 100 的和
+// 需要做尾递归优化
 
 function sum(num) {
   if (num <= 0) {  // 基准条件 base case
@@ -7,4 +8,17 @@ function sum(num) {
   // 递归条件 recursive case
   return num + sum(num - 1)
 }
+
 console.log(sum(100))
+
+// 尾递归优化
+// 函数调用通过参数传递进来，不依赖上一层技术结果
+// 可以通过 curry 处理
+function sum1(num, total) {
+  if (num <= 0) {
+    return total
+  }
+  return sum(num, total)
+}
+
+console.log(sum1(100, 0))
