80732c7beec48fdfd940a0d49fa83ee8692898de
micjoyce <micjoyce90@gmail.com>
2019-04-09 20:31:48 +0800

    123

---
+++ insertSort.js
@@ -0,0 +1,21 @@
+// 插入排序
+
+function insertionSort(arr) {
+  var len = arr.length;
+  var preIndex, current;
+  for (var i = 1; i < len; i++) {
+      preIndex = i - 1;
+      current = arr[i];
+      while (preIndex >= 0 && arr[preIndex] > current) {
+          arr[preIndex + 1] = arr[preIndex];
+          preIndex--;
+      }
+      arr[preIndex + 1] = current;
+  }
+  return arr;
+}
+
+
+var arr = [3, 19, 4, 6, 8, 10, 8, 9]
+
+console.log(insertionSort(arr))
--- selectSort.js
+++ selectSort.js
@@ -1,30 +1,22 @@
 // 选择排序的事件复杂度为 O(n^2)
 
-// 找出数组中最小的值
-function getSmallIdx(arr) {
-  var small = arr[0]
-  var smallIdx = 0
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < small) {
-      small = arr[i]
-      smallIdx = i
+function selectionSort(arr) {
+  var len = arr.length;
+  var minIndex, temp;
+  for (var i = 0; i < len - 1; i++) {
+    minIndex = i;
+    for (var j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) { // 寻找最小的数
+        minIndex = j; // 将最小数的索引保存
+      }
     }
+    temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
   }
-  return smallIdx
+  return arr;
 }
 
-function selectSort(arr) {
-  var newArr = []
-  var len = arr.length
-  for (let i = 0; i < len; i++) {
-    console.info(i)
-    var smallIndex = getSmallIdx(arr)
-    newArr.push(arr[smallIndex]) // 更新到新的数组中
-    arr.splice(smallIndex, 1) // 通过 splice 删除数组
-  }
-  return newArr
-}
+var arr = [3, 19, 4, 6, 8, 10, 8, 9]
 
-var arr = [3, 19, 4, 6, 8 ,10, 8, 9]
-
-console.log(selectSort(arr))
+console.log(selectionSort(arr))
